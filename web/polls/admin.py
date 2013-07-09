a00efc81165a9067ea9741b232eae22006a2b8b2
Richard Allen <rsaxvc@gmail.com>
2013-07-08 22:16:57 -0500

    Partway through tut4

---
+++ web/polls/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from polls.models import Choice
+
+admin.site.register(Choice)
+
+from polls.models import Poll
+
+class PollAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    list_display = ('question', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question']
+    date_hierarchy = 'pub_date'
+
+admin.site.register(Poll, PollAdmin)
+
