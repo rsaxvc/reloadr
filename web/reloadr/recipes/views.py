e4bc096ae9c90df25279e774123372dcea2d1804
Richard Allen <rsaxvc@gmail.com>
2013-07-14 22:54:56 -0500

    Start on front-end

--- web/reloadr/recipes/views.py
+++ web/reloadr/recipes/views.py
@@ -1,1 +1,30 @@
+from django.template import RequestContext, loader
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.core.urlresolvers import reverse
+from django.views import generic
+
+from django.utils import timezone
+
+from recipes.models import Recipe
+
 # Create your views here.
+class IndexView(generic.ListView):
+	template_name = 'recipes/index.html'
+	context_object_name = 'recipe_list'
+
+	def get_queryset(self):
+		"""Return the last five published recipes."""
+		return Recipe.objects
+#		return Recipe.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Recipe
+    template_name = 'recipes/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any recipes that aren't published yet.
+        """
+        return Recipe.objects
+#        return .objects.filter(pub_date__lte=timezone.now())
--- web/reloadr/reloadr/urls.py
+++ web/reloadr/reloadr/urls.py
@@ -5,13 +5,14 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'reloadr.views.home', name='home'),
-    # url(r'^reloadr/', include('reloadr.foo.urls')),
+	# Examples:
+	# url(r'^$', 'reloadr.views.home', name='home'),
+	# url(r'^reloadr/', include('reloadr.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+	# Uncomment the admin/doc line below to enable admin documentation:
+	# url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+	# Uncomment the next line to enable the admin:
+	url(r'^admin/', include(admin.site.urls)),
+	url(r'^recipes/', include('recipes.urls',namespace='recipes')),
 )
