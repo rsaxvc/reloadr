988ae4832f022fa82cd2a500543bddbd09393e8a
Richard Allen <rsaxvc@gmail.com>
2013-07-27 23:33:35 -0500

    Revert "Add some primary key restrictions"
    This removes the id column, which is a bummer
    
    This reverts commit d9e5c7fa927bc108deb55ef55a32fb3479421e5c.

--- web/reloadr/recipes/models.py
+++ web/reloadr/recipes/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 
 class Manufacturer(models.Model):
-	name = models.CharField(max_length=200,primary_key=True)
+	name = models.CharField(max_length=200)
 	url = models.CharField(max_length=2048,blank=True)
 	def __unicode__(self):
 		return self.name
 
 class Gauge(models.Model):
-	gauge = models.CharField(max_length=20,primary_key=True)
+	size = models.CharField(max_length=20)
 	def __unicode__(self):
-		return str(self.gauge) + ' Gauge'
+		return str(self.size) + ' Gauge'
 
 class Primer(models.Model):
 	manufacturer = models.ForeignKey(Manufacturer)
@@ -27,7 +27,7 @@ class Wad(models.Model):
 		return unicode(self.manufacturer) + ' ' + self.name
 
 class Length(models.Model):
-	length = models.CharField(max_length=20,primary_key=True)
+	length = models.CharField(max_length=20)
 	def __unicode__(self):
 		return self.length + ' Inch'
 
